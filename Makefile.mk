41ee01aef8d4554a79c8e5cb3487621e67a4e432
Abbey Skinner <abskin@unc.edu>
2024-10-07 14:13:23 -0400

    created Makefile

---
+++ Makefile.mk
@@ -0,0 +1,18 @@
+.PHONY: clean
+.PHONY: init
+
+init:
+	mkdir -p derived_data
+	mkdir -p figures
+	mkdir -p logs
+
+clean:
+	rm -rf derived_data
+	rm -rf figures
+	rm -rf logs
+	mkdir -p derived_data
+	mkdir -p figures
+	mkdir -p logs
+
+report.Rmd: 
+	Rscript
