1d43ab5234fc4ec50323dfac771c47c94619816e
Abbey Skinner <abskin@unc.edu>
2024-10-08 17:52:48 -0400

    updated makefile

--- Makefile.mk
+++ Makefile.mk
@@ -10,9 +10,10 @@ clean:
 	rm -rf derived_data
 	rm -rf figures
 	rm -rf logs
+	rm -rf report.html
 	mkdir -p derived_data
 	mkdir -p figures
 	mkdir -p logs
 
-report.Rmd: 
-	Rscript
+report.html: report.Rmd
+	Rscript -e "rmarkdown::render('report.Rmd', output_file = 'report.html')"
