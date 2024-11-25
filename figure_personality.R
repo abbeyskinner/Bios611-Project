3e87d83ecd6f416c490422275daee5a694d67987
Abbey Skinner <abskin@unc.edu>
2024-11-25 00:07:35 +0000

    Rscript for Myers Briggs personality types

---
+++ figure_personality.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(survivoR)
+library(ggplot2)
+
+contestant_data <- read_csv("derived_data/contestant_data.csv")
+
+figure_personality <- 
+  ggplot(contestant_data %>% drop_na(personality_type), 
+         aes(x = fct_reorder(personality_type, normalized_finish, .fun = median), 
+             y = normalized_finish, fill = mb_ei)) +
+  geom_boxplot(outlier.color = "red", alpha = 0.8) +
+  coord_flip() +
+  labs(title = "Survivor Placement Percentile by Personality Type",
+       x = "Personality Type",
+       y = "Placement Percentile",
+       fill = "Personality") +
+  theme_minimal() +
+  geom_hline(yintercept = mean(contestant_data$normalized_finish), 
+             color = "red", size = 1) #+  # Add  line at overall mean
+
+ggsave("figures/personality.png", figure_personality)
